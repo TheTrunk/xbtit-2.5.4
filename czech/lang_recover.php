f2e8f0da9c38fb18d027d8f946603859558cf906
Tadeáš Kmenta <TheTrunk@users.noreply.github.com>
2017-06-17 21:01:37 +0200

    Update lang_recover.php

--- czech/lang_recover.php
+++ czech/lang_recover.php
@@ -1,9 +1,9 @@
 <?php
-$language['ERR_NO_EMAIL']='You must provide an email address';
-$language['ERR_INV_EMAIL']='You must enter a valid email address';
-$language['ERR_NO_CAPTCHA']='You must enter the ImageCode';
-$language['IMAGE_CODE']='Image Code';
-$language['SECURITY_CODE']='Answer the question';
-$language['RECOVER_EMAIL_1']="\n".'Someone, hopefully you, requested that the password for the account associated with this email address (%s) be reset.'."\n\n".'The request originated from %s.'."\n\n".'If you did not do this ignore this email. Please do not reply.'."\n\n".'Should you wish to confirm this request, please follow this link:'."\n\n".'%s'."\n\n".'After you do this, your password will be reset and emailed back to you.'."\n--\n".'%s';
-$language['RECOVER_EMAIL_2']="\n".'As per your request we have generated a new password for your account.'."\n\n".'Here is the information we now have on file for this account:'."\n\n\t".'User name: %s'."\n\n\t".'Password: %s'."\n\n".'You may login at %s'."\n\n--\n".'%s';
+$language['ERR_NO_EMAIL']='Musíte zadat email';
+$language['ERR_INV_EMAIL']='Vložte platný email';
+$language['ERR_NO_CAPTCHA']='Vložte obrázkový kód';
+$language['IMAGE_CODE']='Obrázkový kód';
+$language['SECURITY_CODE']='Odpovězte na otázku';
+$language['RECOVER_EMAIL_1']="\n".'Pokoušíte se resetovat heslo k účtu spojeném s emailovou adresou (%s) .'."\n\n".'Požadavek přišel z %s.'."\n\n".'Jestliže jste to nebyli vy, ignorujte tento email. Prosím neodpovídejte.'."\n\n".'Pro resetování hesla prosím klikněte na následující odkaz:'."\n\n".'%s'."\n\n".'Heslo bude vyresetováno a přihlašovací údaje budou obratem zaslány emailem.'."\n--\n".'%s';
+$language['RECOVER_EMAIL_2']="\n".'Na základě vaší žádosti jsme vygenerovali nové heslo k vašemu účtu.'."\n\n".'Přihlašovací údaje k účtu:'."\n\n\t".'Uživatelské jméno: %s'."\n\n\t".'Heslo: %s'."\n\n".'Můžete se přihlásit zde: %s'."\n\n--\n".'%s';
 ?>
