d647edab82e97d02ddb6804b7548c597375da995
Tadeáš Kmenta <TheTrunk@users.noreply.github.com>
2017-06-17 20:22:04 +0200

    Update lang_usercp.php

--- czech/lang_usercp.php
+++ czech/lang_usercp.php
@@ -1,14 +1,14 @@
 <?php
-$language['DELETE_READED']='Delete';
-$language['USER_LANGUE']='Language';
-$language['USER_STYLE']='Style';
-$language['CURRENTLY_PEER']='You&rsquo;re currently seeding or leeching some torrent.';
-$language['STOP_PEER']='You must stop your client.';
-$language['USER_PWD_AGAIN']='Repeat password';
-$language['EMAIL_FAILED']='Sending email has failed!';
-$language['NO_SUBJECT']='No subject';
-$language['MUST_ENTER_PASSWORD']='<br /><font color="#FF0000"><strong>You must enter your password to change the settings above.</strong></font>';
-$language['ERR_PASS_WRONG']='Password empty or incorrect, cannot update profile.';
-$language['MSG_DEL_ALL_PM']='If you select PMs which are not read, they will not be deleted';
-$language['ERR_PM_GUEST']='Sorry you can&rsquo;t send PM to guest or to yourself!';
+$language['DELETE_READED']='Smazat';
+$language['USER_LANGUE']='Jazyk';
+$language['USER_STYLE']='Styl';
+$language['CURRENTLY_PEER']='Momentálně seedujete nebo leechujete nějaký torrent.';
+$language['STOP_PEER']='Musíte ukončit klienta.';
+$language['USER_PWD_AGAIN']='Zopakovat heslo';
+$language['EMAIL_FAILED']='Odeslání emailu selhalo!';
+$language['NO_SUBJECT']='Žádný předmět';
+$language['MUST_ENTER_PASSWORD']='<br /><font color="#FF0000"><strong>Pro provedení změny nastavení vložte heslo.</strong></font>';
+$language['ERR_PASS_WRONG']='Heslo nezadáno nebo je chybné, profil nelze aktualizovat.';
+$language['MSG_DEL_ALL_PM']='Jestliže vyberete zprávy (PMs), které nebyly přečteny, nebudou smazány';
+$language['ERR_PM_GUEST']='Nelze odeslat zprávu (PM) sám sobě nebo uživateli hosta!';
 ?>
